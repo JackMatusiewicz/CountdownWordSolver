7674d271ae6f5f4edc3fc8fbef685cb295944076
Jack <jack.matusiewicz@outlook.com>
2017-05-06 00:17:02 +0100

    Initial commit of hacked together Trie to store the word dictionary.

---
+++ CountdownWordSolver/Program.fs
@@ -0,0 +1,27 @@
+﻿    // Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open Trie
+
+let getLinesFromFile (path : string) = seq {
+    use reader = new System.IO.StreamReader(path)
+    while reader.EndOfStream = false do
+        yield reader.ReadLine()
+}
+
+let filterInvalidWords (words : string seq) =
+    let regex = System.Text.RegularExpressions.Regex("[a-z]")
+    words
+        |> Seq.map (fun w -> w.ToLower())
+        |> Seq.filter (fun w -> regex.Match(w).Success)
+
+let getValidWordsFromFile = getLinesFromFile >> filterInvalidWords
+
+[<EntryPoint>]
+let main argv =
+    let trie = "/usr/share/dict/words" |> getValidWordsFromFile |> Trie.buildTrie
+
+    printfn "%A" <| Trie.contains "accenture" trie
+
+
+    0 // return an integer exit code
---
+++ CountdownWordSolver/Trie.fs
@@ -0,0 +1,53 @@
+module Trie
+
+type WordAtNode = WordEndsHere | NoWord
+
+type TrieNode = | Node of Map<char, TrieNode> * WordAtNode
+
+let head = (Node (Map.empty, NoWord))
+
+let addWord (word : string) (trie : TrieNode) =
+    let chars = List.ofSeq word
+
+    let getNode (trieNode : TrieNode) (letter : char) : (TrieNode * Map<char, TrieNode>) =
+        let (Node (map,_)) = trieNode
+        if Map.containsKey letter map then
+            (Map.find letter map, Map.remove letter map)
+        else
+            (Node (Map.empty, NoWord), map)
+
+    let rec add trieNode (chars : char list) =
+        let (Node (map,WordAtNode)) = trieNode
+        match chars with
+        | [] ->
+            Node (map, WordEndsHere)
+        | hd::tl ->
+            let child, updatedMap = getNode trieNode hd
+            let newSubTree = add child tl
+            Node (Map.add hd newSubTree updatedMap, WordAtNode)
+    add trie chars
+
+let buildTrie (words : string seq) =
+    words |> Seq.fold (fun trie word -> addWord word trie) head
+
+let contains (word : string) (trie : TrieNode) =
+    let chars = List.ofSeq word
+
+    let getNode trieNode letter =
+        let (Node (map,_)) = trieNode
+        if Map.containsKey letter map then
+            Map.find letter map |> Some
+        else
+            None
+
+    let rec find trieNode letters =
+        match letters with
+        | [] ->
+            match trieNode with
+            | (Node (_, NoWord))       -> false
+            | (Node (_, WordEndsHere)) -> true
+        | hd :: tl ->
+            match getNode trieNode hd with
+            | Some node -> find node tl
+            | None -> false
+    find trie chars
