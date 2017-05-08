341ace38de7d9e015f1f3b8514487eb48461086e
Jack <jack.matusiewicz@outlook.com>
2017-05-08 22:06:48 +0100

    Adding initial function to find permutations of a list

--- CountdownWordSolver/Permutation.fs
+++ CountdownWordSolver/Permutation.fs
@@ -18,9 +18,26 @@ let permutations (input : 'a list) : ('a list list) =
     and addElementToAllPositionsInList (element : 'a) frontOfList backofList (newLists : 'a list list) =
         match backofList with
         | [] ->
-            [(frontOfList @ [element])] @ newLists
+            (frontOfList @ [element]) :: newLists
         | hd::tl ->
             let updated = frontOfList @ [element] @ [hd] @ tl
             addElementToAllPositionsInList element (frontOfList @ [hd]) tl (updated::newLists)
 
     findAllPermutations input [[]]
+
+//A more concise way to do it - but much slower.
+let addInAllPositions (element : 'a) (data : 'a list) =
+    let rec addValueInAllPositions (element : 'a) frontOfList backofList (newLists : 'a list list) =
+            match backofList with
+            | [] ->
+                (frontOfList @ [element]) :: newLists
+            | hd::tl as l ->
+                let updated = frontOfList @ [element] @ l
+                addValueInAllPositions element (frontOfList @ [hd]) tl (updated::newLists)
+    addValueInAllPositions element [] data []
+
+let rec perms (data : 'a list) =
+    match data with
+    | [] -> []
+    | x::[] -> [[x]]
+    | hd::tl -> List.fold (fun acc p -> acc @ addInAllPositions hd p ) [] (perms tl)
